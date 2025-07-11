d1660542891a9b7dab59d0120cfc7a262ad240fa
Moch Yogi Firmansyah <yogifirmansyah1407@gmail.com>
2025-07-11 14:21:08 +0700

    Add package flutter_local_notifications and timezone, Add permission in android &ios for notification

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,17 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // New
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+      GeneratedPluginRegistrant.register(with: registry)
+      }
+    
     GeneratedPluginRegistrant.register(with: self)
+    
+    // New
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
